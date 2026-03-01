4a69f26e76b4e9c5f220ad0a34e8feec28ce5547
seantrim <strim201325@gmail.com>
2026-03-01 00:07:27 -0600

    Replaced Makefile with meson build scripts.

--- Fortran/build_with_meson.sh
+++ Fortran/build_with_meson.sh
@@ -1,8 +1,10 @@
 #export FC="ifx" # source /opt/intel/oneapi/setvars.sh
-#export FC="gfortran"
+export FC="gfortran"
 #export FFLAGS='-flto -O3' # production
+#export FFLAGS='-Og -ffpe-trap=invalid,zero,overflow,underflow -ggdb3 -fbounds-check -fbacktrace' # debug
 
-rm -rf builddir
-meson setup builddir -Dbuildtype=plain -Dwarning_level=0
+rm -rf builddir # ensures a new build from scratch
+#meson setup builddir -Dbuildtype=release -Dwarning_level=0 -Db_lto=true # production
+meson setup builddir -Dbuildtype=debug -Dwarning_level=everything # debug
 meson compile -C builddir -v
-
+mv builddir/exact_solution_code ./
