a726c98feae63c0a8e4f06510aa0768c255e129f
seantrim <strim201325@gmail.com>
2026-03-01 05:42:23 -0600

    Made progress with meson build script -- but does not work with ifx -ipo optimization.

--- Fortran/build_with_meson.sh
+++ Fortran/build_with_meson.sh
@@ -1,10 +1,33 @@
-#export FC="ifx" # source /opt/intel/oneapi/setvars.sh
-export FC="gfortran"
-#export FFLAGS='-flto -O3' # production
-#export FFLAGS='-Og -ffpe-trap=invalid,zero,overflow,underflow -ggdb3 -fbounds-check -fbacktrace' # debug
+# Input Options
+FC="ifx"     # Fortran compiler: "gfortran" or "ifx" (may need to run source /opt/intel/oneapi/setvars.sh for access to ifx via Intel oneAPI)
+mode="debug" # build mode: "debug" or "production"
+
+# Automated Build Operations Below
+export FC
 
 rm -rf builddir # ensures a new build from scratch
-#meson setup builddir -Dbuildtype=release -Dwarning_level=0 -Db_lto=true # production
-meson setup builddir -Dbuildtype=debug -Dwarning_level=everything # debug
-meson compile -C builddir -v
-mv builddir/exact_solution_code ./
+
+# set up builddir for the selected build mode
+if [[ "$mode" == "debug" ]]; then # additional debug options that are not easily selected with built-in meson options
+  if [[ "$FC" == "gfortran" ]]; then
+    export FFLAGS='-ffpe-trap=invalid,zero,overflow,underflow -fbounds-check -fbacktrace' # floating-point traps and checks for arrays bounds
+  elif [[ "$FC" == "ifx" ]]; then
+    export FFLAGS='-fpe:0 -check all' # floating-point traps and checks for arrays bounds and shape violations
+  else
+    export FFLAGS=''
+  fi
+  meson setup builddir -Dbuildtype=debug -Dwarning_level=everything # debug
+else # apply O3 optimzation to account for meson not applying it at the link stage (bug info: https://github.com/mesonbuild/meson/issues/11318)
+  if [[ "$FC" == "gfortran" ]]; then
+    export FFLAGS='-O3'
+  elif [[ "$FC" == "ifx" ]]; then
+    #export FFLAGS='-O3 -ipo' # apply -ipo option because meson does not apply it automatically with built-in link time optimization option 
+    export FFLAGS='-O3' # neglecting -ipo for now because it is currently not functioning (as of meson 1.7.0 and ifx 2025.3.2 20260112)
+  else
+    export FFLAGS=''
+  fi
+  meson setup builddir -Dbuildtype=release -Dwarning_level=0 -Db_lto=true # production
+fi
+
+meson compile -C builddir -v       # build executable (artifacts stored in builddir)
+mv builddir/exact_solution_code ./ # move executable to current directory
